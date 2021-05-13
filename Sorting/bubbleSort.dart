5e4b0a7c0439f46649b5819b2591d20cccab85cf
Ayush Rawat <ayush002rawat@gmail.com>
2021-05-13 10:34:48 +0530

    bubble sort

---
+++ Sorting/bubbleSort.dart
@@ -0,0 +1,19 @@
+void main() {
+  List<int> input = [6, -5, 3, 1, 8, 7, 3, 4];
+  print(bubbleSort(input));
+}
+
+List<int> bubbleSort(List<int> input) {
+  for (int i = 0; i < input.length; i++) {
+    for (int j = 0; j < input.length; j++) {
+      if ((j + 1) < input.length) {
+        if (input[j] > input[j + 1]) {
+          int temp = input[j];
+          input[j] = input[j + 1];
+          input[j + 1] = temp;
+        }
+      }
+    }
+  }
+  return input;
+}
